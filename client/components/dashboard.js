1c4910c85b4a24dede98f265897fa01fa5eebf78
Никита Лысенко <nikitalysenko@MacBook-Pro-Nikita.local>
2021-01-08 01:14:00 +0300

    add title in dashboard

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div>
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           Dashboard
           <div> <Link to="/dashboard/profile/9ae87d4d-5b0d-4513-9300-ab551b2a2381"> Go to Profile </Link> </div>
           <div> <Link to="/dashboard/main"> Go to Main </Link> </div>

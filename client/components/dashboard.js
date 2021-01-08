8fb97d5dbf6c59dfcb4edb3480ca96bdd0465897
Никита Лысенко <nikitalysenko@MacBook-Pro-Nikita.local>
2021-01-08 13:59:47 +0300

    id trouble

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -8,10 +8,10 @@ const Dashboard = () => {
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          Dashboard
-          <div> <Link to="/dashboard/profile/9ae87d4d-5b0d-4513-9300-ab551b2a2381"> Go to Profile </Link> </div>
-          <div> <Link to="/dashboard/main"> Go to Main </Link> </div>
+        Dashboard 
         </div>
+        <div> <Link to="/dashboard/profile/9ae87d4d-5b0d-4513-9300-ab551b2a2381"> Go to Profile </Link> </div>
+        <div> <Link to="/dashboard/main"> Go to Main </Link> </div>
       </div>
     </div>
   )
--- client/components/profile.js
+++ client/components/profile.js
@@ -10,10 +10,10 @@ const Profile = () => {
       <div className="flex items-center justify-center h-screen">
         <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
       Profile
-          <div> <Link to="/dashboard"> Go to Root </Link> </div>
-          <div> <Link to="/dashboard/main"> Go to Main </Link> </div>
-          <div id="username"> {user} </div>
         </div>
+        <div> <Link to="/dashboard"> Go to Root </Link> </div>
+        <div> <Link to="/dashboard/main"> Go to Main </Link> </div>
+        <div id="username"> {user} </div>
       </div>
     </div>
   )
